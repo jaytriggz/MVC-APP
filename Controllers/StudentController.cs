620714c4a032e6576951f7219c7ab6bfc20a7d01
Dev-triggz <arotroy01@hotmail.com>
2025-03-24 10:06:00 -0400

    ASP MVC is Fun 03/2025

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace StudentApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Sample list of students
+            var students = new List<string>
+            {
+                "Troy",
+                "Nicole",
+                "Charlie",
+                "Beyonce"
+            };
+
+            return View(students); // Pass the list to the view
+        }
+    }
+}
