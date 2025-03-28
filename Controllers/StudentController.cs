b442e480926c3ffbbb3461cbd81cd1fe7677c52c
Dev-triggz <arotroy01@hotmail.com>
2025-03-27 22:08:03 -0400

    fnl

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -14,6 +14,7 @@ namespace StudentApp.Controllers
                 "Nicole",
                 "Charlie",
                 "Femi",
+                "matt and julie",
                 "Beyonce"
             };
 
